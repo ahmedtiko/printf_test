f822e93dc6c7ab5a36db42bb78f2309c8cab0469
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-11-13 00:15:04 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
+void print_buffer(char buffer[], int *buff_index);
 
 /**
  * _printf - Printf function
@@ -60,13 +60,13 @@ int _printf(const char *format, ...)
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_index: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_index)
 {
 	if (*buff_index > 0)
 	{
-		write(1, &buffer[0], *buff_ind);
+		write(1, &buffer[0], *buff_index);
 	}
 
 	*buff_index = 0;
