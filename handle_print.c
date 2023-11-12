bd4d75869f97db89e4d222c155595025cffebb47
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-11-13 01:37:14 +0200

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,30 +1,37 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * handle_print - Prints an argument based on its type
- * @format: Formatted string in which to print the arguments.
- * @ind: Ind.
- * @list: List of arguments to be printed.
+ * handle_print - Handles the printing of formatted output.
+ * @format: Format string.
+ * @ind: Pointer to the index in the format string.
+ * @list: Variable argument list.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: Get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ * @flags: Active flags.
+ * @width: Width specifier.
+ * @precision: Precision specifier.
+ * @size: Size specifier.
+ *
+ * Return: Number of characters printed.
  */
 int handle_print(const char *format, int *ind, va_list list, char buffer[],
-    int flags, int width, int precision, int size)
+                 int flags, int width, int precision, int size)
 {
-    int i, un_len = 0, print_char = -1;
-    format_t format_types[] = {
-    {'c', &print_char}, {'s', &print_string}, {'%', &print_percent},
-    {'i', &print_int}, {'d', &print_int}, {'b', &print_binary},
-    {'u', &print_unsigned}, {'o', &print_octal}, {'x', &print_hexadecimal},
-    {'X', &print_hexa}, {'p', &print_pointer}, {'S', &print_non_printable},
-    {'r', &print_reverse}, {'R', &print_rot13string}, {'\0', NULL}
-};
+    int i;
+    int un_len = 0;
+    int print_char = -1;
 
+    struct format
+    {
+        char fmt;
+        int (*fn)(va_list, char[], int, int, int, int);
+    };
+
+    struct format format_types[] = {
+        {'c', &print_char}, {'s', &print_string}, {'%', &print_percent},
+        {'i', &print_int}, {'d', &print_int}, {'b', &print_binary},
+        {'u', &print_unsigned}, {'o', &print_octal}, {'x', &print_hexadecimal},
+        {'X', &print_hexa}, {'p', &print_pointer}, {'S', &print_non_printable},
+        {'r', &print_reverse}, {'R', &print_rot13string}, {'\0', NULL}
     };
 
     for (i = 0; format_types[i].fmt != '\0'; i++)
@@ -35,17 +42,15 @@ int handle_print(const char *format, int *ind, va_list list, char buffer[],
         }
     }
 
-    if (format_types[i].fmt == '\0')
+    /* Handle other cases here */
+    if (format[*ind] == '\0')
     {
-        if (format[*ind] == '\0')
-        {
-            return -1;
-        }
-
-        /* Handle other cases here*/
-
-        return un_len;
+        return 0; /* End of format string */
     }
-
-    return print_char;
+    else
+    {
+        buffer[0] = format[*ind];
+        buffer[1] = '\0';
+        return 1; /* Single character printed */
+    }
 }
