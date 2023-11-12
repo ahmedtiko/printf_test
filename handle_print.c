fd4bb843af93a76cf141dc2c3b3ea97b9c7d83c4
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-11-13 01:30:46 +0200

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -18,11 +18,13 @@ int handle_print(const char *format, int *ind, va_list list, char buffer[],
 {
     int i, un_len = 0, print_char = -1;
     format_t format_types[] = {
-        {'c', print_char}, {'s', print_string}, {'%', print_percent},
-        {'i', print_int}, {'d', print_int}, {'b', print_binary},
-        {'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-        {'X', print_HEXA}, {'p', print_pointer}, {'S', print_non_printable},
-        {'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+    {'c', &print_char}, {'s', &print_string}, {'%', &print_percent},
+    {'i', &print_int}, {'d', &print_int}, {'b', &print_binary},
+    {'u', &print_unsigned}, {'o', &print_octal}, {'x', &print_hexadecimal},
+    {'X', &print_hexa}, {'p', &print_pointer}, {'S', &print_non_printable},
+    {'r', &print_reverse}, {'R', &print_rot13string}, {'\0', NULL}
+};
+
     };
 
     for (i = 0; format_types[i].fmt != '\0'; i++)
