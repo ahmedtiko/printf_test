21677226d88dabdd3a85b73aa6c79aa26f711912
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-11-14 01:29:00 +0200

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -10,52 +10,7 @@
 * Return: Number of chars printed.
 */
 int hand_write_ch(char c, char buffer[],
-
-	int flags, int width, int precision, int size)
-{
-	int i, y = 0;
-	char padd = ' ';
-
-	UNUSED(precision);
-	UNUSED(size);
-
-	if (flags & FLAG_ZERO)
-	{
-		padd = '0';
-	}
-
-	buffer[y++] = c;
-	buffer[y] = '\0';
-
-	if (width > 1)
-	{
-		int remainingWidth = width - 1;
-		int writeCount;
-		buffer[BUFF_SIZE - 1] = '\0';
-
-		for (i = 0; i < remainingWidth; i++)
-		{
-			buffer[BUFF_SIZE - i - 2] = padd;
-		}
-
-		if (flags & FLAG_MINUS)
-		{
-			 writeCount = write(1, &buffer[0], 1) + write(1, &buffer[BUFF_SIZE - remainingWidth - 1], remainingWidth);
-			/*return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));*/
-		}
-		else
-		{
-			writeCount = write(1, &buffer[BUFF_SIZE - remainingWidth - 1], remainingWidth) + write(1, &buffer[0], 1);
-			/*return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
-					write(1, &buffer[0], 1));*/
-		}
-		return (writeCount);
-	}
-
-	return (write(1, &buffer[0], 1));
-}
-int (flags, int width, int precision, int size)
+int flags, int width, int precision, int size)
 {
 int i, y = 0;
 char padd = ' ';
@@ -67,6 +22,7 @@ padd = '0';
 }
 buffer[y++] = c;
 buffer[y] = '\0';
+
 if (width > 1)
 {
 buffer[BUFF_SIZE - 1] = '\0';
@@ -120,9 +76,11 @@ else if (flags & FLAG_SPACE)
 {
 extra_ch = ' ';
 }
+
 return (write_num(ind, buffer, flags, width, precision,
 len, padd, extra_ch));
 }
+
 /**
 * write_num - Write a number using a buffer.
 * @ind: Index at which the number starts on the buffer.
@@ -141,6 +99,7 @@ int flags, int width, int prec,
 int length, char padd, char extra_c)
 {
 int i, padd_start = 1;
+
 if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 {
 return (0);
@@ -174,6 +133,7 @@ if (extra_c)
 {
 buffer[--ind] = extra_c;
 }
+
 return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 }
 else if (!(flags & FLAG_MINUS) && padd == ' ')
@@ -200,6 +160,7 @@ buffer[--ind] = extra_c;
 }
 return (write(1, &buffer[ind], length));
 }
+
 /**
 * write_unsgnd - Writes an unsigned number.
 * @is_negative: Number indicating if the num is negative.
@@ -253,6 +214,7 @@ else /* Asign extra char to left of padding [padd>buffer]*/
 return (write(1, &buffer[0], i) + write(1, &buffer[ind], len));
 }
 }
+
 return (write(1, &buffer[ind], len));
 }
 /**
@@ -278,6 +240,7 @@ for (i = 3; i < width - length + 3; i++)
 {
 buffer[i] = padd;
 }
+
 buffer[i] = '\0';
 if (flags & FLAG_MINUS && padd == ' ')
 {
