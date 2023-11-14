6579907db77aaa92d04b44d4b4883838eadc8bdc
GeorgeGhabour22 <95974387+GeorgeGhabour22@users.noreply.github.com>
2023-11-14 05:42:42 +0200

    Update main.h

--- main.h
+++ main.h
@@ -86,7 +86,7 @@ int hand_write_ch(char c, char buffer[],
 int write_num_0(int is_positive, int ind, char buffer[],
 	int flags, int width, int precision, int size);
 int write_num(int ind, char bff[], int flags, int width, int precision,
-	int length, char padd, char extra_c);
+	int length, char padd, char extra_c, int is_negative);
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsgnd(int is_negative, int ind,
